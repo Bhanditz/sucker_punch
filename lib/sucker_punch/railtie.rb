2d067a8c8edc682a7d6925b18cf35c7dcc8c1576
notmaxx <alexey.pikin@gmail.com>
2017-02-21 20:30:04 -0800

    fix

--- lib/sucker_punch/railtie.rb
+++ lib/sucker_punch/railtie.rb
@@ -3,9 +3,9 @@ module SuckerPunch
     initializer "sucker_punch.logger" do
       SuckerPunch.logger = Rails.logger
     end
-  end
 
-  config.to_prepare do
-    Celluloid::Actor.clear_registry
+    config.to_prepare do         
+      Celluloid::Actor.clear_registry 
+    end
   end
 end
